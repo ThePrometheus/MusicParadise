977071f61d30abf6f07303c7986457346311d514
ThePrometheus <nazartkach3@gmail.com>
2017-04-11 02:01:51 +0300

    Added basic CRUD to Consultant class

--- src/main/java/components/Consultant.java
+++ src/main/java/components/Consultant.java
@@ -9,23 +9,94 @@ public class Consultant implements User {
     private String password;
     private String firstname;
     private String middlename;
-    private String job;
+
     private String birth_date;
     private String telephone_number;
+
+    @Override
+    public String toString() {
+        return "Consultant{" +
+                "id=" + id +
+                ", surname='" + surname + '\'' +
+                ", password='" + password + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", middlename='" + middlename + '\'' +
+                ", birth_date='" + birth_date + '\'' +
+                ", telephone_number='" + telephone_number + '\'' +
+                ", department_id=" + department_id +
+                ", login='" + login + '\'' +
+                ", role='" + role + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Consultant that = (Consultant) o;
+
+        if (id != that.id) return false;
+        if (department_id != that.department_id) return false;
+        if (Double.compare(that.salary, salary) != 0) return false;
+        if (surname != null ? !surname.equals(that.surname) : that.surname != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        if (middlename != null ? !middlename.equals(that.middlename) : that.middlename != null) return false;
+        if (birth_date != null ? !birth_date.equals(that.birth_date) : that.birth_date != null) return false;
+        if (telephone_number != null ? !telephone_number.equals(that.telephone_number) : that.telephone_number != null)
+            return false;
+        if (login != null ? !login.equals(that.login) : that.login != null) return false;
+        return role != null ? role.equals(that.role) : that.role == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+        result = 31 * result + (middlename != null ? middlename.hashCode() : 0);
+        result = 31 * result + (birth_date != null ? birth_date.hashCode() : 0);
+        result = 31 * result + (telephone_number != null ? telephone_number.hashCode() : 0);
+        result = 31 * result + department_id;
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        temp = Double.doubleToLongBits(salary);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
+
+    private int department_id;
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
     private String login;
 
-    public Consultant(int id, String surname, String password, String firstname, String middlename, String job, String birth_date, String telephone_number, String login, double salary) {
-        this.id = id;
-        this.surname = surname;
-        this.password = password;
-        this.firstname = firstname;
-        this.middlename = middlename;
-        this.job = job;
-        this.birth_date = birth_date;
-        this.telephone_number = telephone_number;
-        this.login = login;
-        this.salary = salary;
+    public String getRole() {
+        return role;
     }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    private String role;
+
+
 
     public int getId() {
         return id;
@@ -71,13 +142,6 @@ public class Consultant implements User {
         this.middlename = middlename;
     }
 
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
 
     public String getBirth_date() {
         return birth_date;
@@ -106,57 +170,4 @@ public class Consultant implements User {
     private double salary;
 
 
-    @Override
-    public String toString() {
-        return "Consultant{" +
-                "id=" + id +
-                ", surname='" + surname + '\'' +
-                ", password='" + password + '\'' +
-                ", firstname='" + firstname + '\'' +
-                ", middlename='" + middlename + '\'' +
-                ", job='" + job + '\'' +
-                ", birth_date='" + birth_date + '\'' +
-                ", telephone_number='" + telephone_number + '\'' +
-                ", login='" + login + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Consultant that = (Consultant) o;
-
-        if (id != that.id) return false;
-        if (Double.compare(that.salary, salary) != 0) return false;
-        if (surname != null ? !surname.equals(that.surname) : that.surname != null) return false;
-        if (password != null ? !password.equals(that.password) : that.password != null) return false;
-        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-        if (middlename != null ? !middlename.equals(that.middlename) : that.middlename != null) return false;
-        if (job != null ? !job.equals(that.job) : that.job != null) return false;
-        if (birth_date != null ? !birth_date.equals(that.birth_date) : that.birth_date != null) return false;
-        if (telephone_number != null ? !telephone_number.equals(that.telephone_number) : that.telephone_number != null)
-            return false;
-        return login != null ? login.equals(that.login) : that.login == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = id;
-        result = 31 * result + (surname != null ? surname.hashCode() : 0);
-        result = 31 * result + (password != null ? password.hashCode() : 0);
-        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
-        result = 31 * result + (middlename != null ? middlename.hashCode() : 0);
-        result = 31 * result + (job != null ? job.hashCode() : 0);
-        result = 31 * result + (birth_date != null ? birth_date.hashCode() : 0);
-        result = 31 * result + (telephone_number != null ? telephone_number.hashCode() : 0);
-        result = 31 * result + (login != null ? login.hashCode() : 0);
-        temp = Double.doubleToLongBits(salary);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
 }
--- src/main/java/dao/DAOimplementation/ConsultantDAOImpl.java
+++ src/main/java/dao/DAOimplementation/ConsultantDAOImpl.java
@@ -1,7 +1,97 @@
 package dao.DAOimplementation;
 
+import components.Consultant;
+import dao.ConsultantDAO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by nazar on 11.04.17.
  */
-public class ConsultantDAOImpl {
+public class ConsultantDAOImpl implements ConsultantDAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private static Logger logger = LoggerFactory.getLogger(ConsultantDAOImpl.class.getSimpleName());
+    private static final String GET = "SELECT  * FROM consultants WHERE id=?";
+    private static final String INSERT = "INSERT INTO consultants (encrypted_password,surname,firstname,middlename,birth_date,tel_number,salary,department,login,role) VALUES (?,?,?,?,?,?,?,?,?,?)";
+    private static final String UPDATE = "UPDATE consultants SET encrypted_password=?,surname=?,firstname=?,middlename=?,birth_date=?,tel_number=?,salary=?,department=?,login=?,role=? WHERE id=?";
+    private static final String DELETE = "DELETE FROM consutlants WHERE id=?";
+
+
+
+    public Consultant get(int id) {
+        logger.info("Conslutant retrieved");
+        return jdbcTemplate.queryForObject(GET,mapper,id);
+    }
+
+    public int insert(Consultant consultant) {
+        logger.info("Client inserted");
+        return jdbcTemplate.update(INSERT,
+                consultant.getPassword(),
+                consultant.getSalary(),
+                consultant.getFirstname(),
+                consultant.getMiddlename(),
+                consultant.getBirth_date(),
+                consultant.getTelephone_number(),
+                consultant.getSalary(),
+                consultant.getLogin(),
+                consultant.getRole(),
+                consultant.getDepartment_id());
+    }
+
+    public void update(Consultant consultant) {
+        logger.info("Consultant updated");
+        jdbcTemplate.update(UPDATE,
+                consultant.getPassword(),
+                consultant.getSurname(),
+                consultant.getFirstname(),
+                consultant.getMiddlename(),
+                consultant.getBirth_date(),
+                consultant.getTelephone_number(),
+                consultant.getSalary(),
+                consultant.getLogin(),
+                consultant.getRole());
+    }
+
+        public void delete(Consultant consultant) {
+        logger.info("Consultant is  deleted");
+        jdbcTemplate.update(DELETE, consultant.getId());
+
+    }
+
+    private RowMapper<Consultant> mapper = new RowMapper<Consultant>() {
+        public Consultant mapRow(ResultSet resultSet, int i) throws SQLException {
+            Consultant consultant = new Consultant();
+            consultant.setId(resultSet.getInt("id"));
+            consultant.setPassword(resultSet.getString("encrypted_password"));
+            consultant.setSurname(resultSet.getString("surname"));
+            consultant.setFirstname(resultSet.getString("firstname"));
+            consultant.setMiddlename(resultSet.getString("middlename"));
+            consultant.setRole(resultSet.getString("role"));
+            consultant.setBirth_date((resultSet.getString("birth_date")));
+            consultant.setTelephone_number(resultSet.getString("tel_number"));
+            consultant.setSalary(resultSet.getShort("salary"));
+            consultant.setLogin(resultSet.getString("login"));
+            consultant.setDepartment_id(resultSet.getShort("department"));
+
+           return consultant;
+
+        }
+
+
+
+
+
+
+    };
 }
+
+
+
